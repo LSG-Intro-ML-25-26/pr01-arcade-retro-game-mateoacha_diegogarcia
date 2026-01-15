314739830f796fac62d03c8bf9de6dae23c9ad26
Mateo Achá <157355023+EseEneZeta@users.noreply.github.com>
2026-01-15 17:45:19 +0100

    Inicio del juego THE ALCHEMIST

--- main.py
+++ main.py
@@ -1,41 +1,421 @@
-def on_up_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-up
-            """),
-        500,
-        False)
-controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
+"""
+👑 THE ALCHEMIST👑
+"""
 
-def on_left_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-left
-            """),
-        500,
-        False)
-controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
+# --- 1. CLASES ---
+class ItemJuego:
+    def __init__(self, nombre: str, imagen: Image, tipo: str):
+        self.nombre = nombre
+        self.imagen = imagen
+        self.tipo = tipo # "mision" o "curacion"
+        self.recogido = False
+        self.sprite_fisico = None
 
-def on_right_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-right
-            """),
-        500,
-        False)
-controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+# --- 2. CONFIGURACIÓN ---
+items: List[ItemJuego] = []
+enemigos: List[Sprite] = []
+jugador: Sprite = None
+rey_npc: Sprite = None
 
-def on_down_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-down
-            """),
-        500,
-        False)
-controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
+energia = 100.0
+juego_activo = False
+mision_iniciada = False
 
-nena: Sprite = None
-nena = sprites.create(assets.image("""
-    nena-front
-    """), SpriteKind.player)
-controller.move_sprite(nena)
+# Tipos de Sprite
+KIND_ITEM = SpriteKind.create()
+KIND_META = SpriteKind.create()
+KIND_ENEMIGO = SpriteKind.enemy
+KIND_NPC = SpriteKind.create()
+
+# --- 3. ARTE PIXEL ---
+
+img_hero = img("""
+    . . . . 2 2 2 2 . . . .
+    . . . 2 2 2 2 2 2 . . .
+    . . 2 2 2 2 2 2 2 2 . .
+    . . 2 2 8 8 8 8 2 2 . .
+    . . 2 8 8 8 8 8 8 2 . .
+    . . 8 8 f f f f 8 8 . .
+    . . 8 f f 2 2 f f 8 . .
+    . . 8 f f f f f f 8 . .
+    . . 8 8 8 8 8 8 8 8 . .
+    . . f 8 8 8 8 8 8 f . .
+    . . f 8 5 5 5 5 8 f . .
+    . . . 8 5 5 5 5 8 . . .
+    . . . f f . . f f . . .
+    . . . . . . . . . . . .
+""")
+
+img_rey = img("""
+    . . . . 5 5 5 5 . . . .
+    . . . 5 5 4 4 5 5 . . .
+    . . 5 5 4 5 5 4 5 5 . .
+    . . 5 5 4 4 4 4 5 5 . .
+    . . . f f f f f f . . .
+    . . . f f 2 2 f f . . .
+    . . . f f f f f f . . .
+    . . . 5 5 5 5 5 5 . . .
+    . . . 5 4 5 5 4 5 . . .
+    . . . 5 5 5 5 5 5 . . .
+    . . . 5 5 5 5 5 5 . . .
+    . . . 5 5 . . 5 5 . . .
+    . . . . . . . . . . . .
+    . . . . . . . . . . . .
+""")
+
+img_fantasma = img("""
+    . . . . . . . . . . . .
+    . . . . 1 1 1 1 . . . .
+    . . . 1 1 1 1 1 1 . . .
+    . . 1 1 1 1 1 1 1 1 . .
+    . . 1 1 f 1 1 f 1 1 . .
+    . . 1 1 1 1 1 1 1 1 . .
+    . . 1 1 1 1 1 1 1 1 . .
+    . . 1 1 1 1 1 1 1 1 . .
+    . . 1 1 1 1 1 1 1 1 . .
+    . . 1 . 1 . 1 . 1 . . .
+    . . . . . . . . . . . .
+""")
+
+img_gema = img("""
+    . . . . . . . . . . . .
+    . . . . . 2 2 . . . . .
+    . . . . 2 4 4 2 . . . .
+    . . . 2 4 2 2 4 2 . . .
+    . . . 2 4 2 2 4 2 . . .
+    . . . 2 4 4 4 4 2 . . .
+    . . . . 2 4 4 2 . . . .
+    . . . . . 2 2 . . . . .
+    . . . . . . . . . . . .
+""")
+
+img_planta = img("""
+    . . . . . . . . . . . .
+    . . . . . . 7 . . . . .
+    . . . . . 7 7 . . . . .
+    . . . 7 . 7 7 . 7 . . .
+    . . . 7 7 7 7 7 7 . . .
+    . . . . 7 7 7 7 . . . .
+    . . . . 7 7 7 7 . . . .
+    . . . . . 7 7 . . . . .
+    . . . . . . . . . . . .
+""")
+
+img_libro = img("""
+    . . . . . . . . . . . .
+    . . . b b b b . . . . .
+    . . b 1 1 1 1 b . . . .
+    . b 1 1 1 1 1 1 b . . .
+    . b c c c c c c b . . .
+    . b c c c c c c b . . .
+    . b c c c c c c b . . .
+    . b 1 1 1 1 1 1 b . . .
+    . . b b b b b b . . . .
+""")
+
+img_salud = img("""
+    . . . . . . . . . . . .
+    . . . . . 1 1 . . . . .
+    . . . . . 1 1 . . . . .
+    . . . . 1 1 1 1 . . . .
+    . . . 1 2 2 2 2 1 . . .
+    . . . 1 2 2 2 2 1 . . .
+    . . 1 2 2 2 2 2 2 1 . .
+    . . 1 2 f f f 2 2 1 . .
+    . . 1 2 2 2 2 2 2 1 . .
+    . . . 1 2 2 2 2 1 . . .
+    . . . . 1 1 1 1 . . . .
+""")
+
+img_caldero = img("""
+    . . . . . . . . . . . .
+    . . . b b . . b b . . .
+    . . b 1 1 b b 1 1 b . .
+    . b 1 1 1 1 1 1 1 1 b .
+    . b 1 2 2 5 5 2 2 1 b .
+    . b 1 2 5 5 5 5 2 1 b .
+    . b b 2 5 5 5 5 2 b b .
+    . . b 2 2 5 5 2 2 b . .
+    . . . b b b b b b . . .
+    . . . d . . . . d . . .
+    . . d . . . . . . d . .
+""")
+
+img_suelo = img("""
+    c c b c c c b c c c b c c c b c
+    c c b c c c b c c c b c c c b c
+    b b b b b b b b b b b b b b b b
+    c b c c c b c c c b c c c b c c
+    c b c c c b c c c b c c c b c c
+    b b b b b b b b b b b b b b b b
+    c c c b c c c b c c c b c c c b
+    c c c b c c c b c c c b c c c b
+    b b b b b b b b b b b b b b b b
+    c c b c c c b c c c b c c c b c
+    c c b c c c b c c c b c c c b c
+    b b b b b b b b b b b b b b b b
+    c b c c c b c c c b c c c b c c
+    c b c c c b c c c b c c c b c c
+    b b b b b b b b b b b b b b b b
+    c c c b c c c b c c c b c c c b
+""")
+
+img_pared = img("""
+    b b b b b b b b b b b b b b b b
+    b d d d d d d d d d d d d d d b
+    b d c c c c c c c c c c c c d b
+    b d c b b b c b b b c b b b c b
+    b d c b b b c b b b c b b b c b
+    b d c c c c c c c c c c c c c b
+    b d c b b b c b b b c b b b c b
+    b d c c c c c c c c c c c c c b
+    b d c c c c c c c c c c c c c b
+    b d c b b b c b b b c b b b c b
+    b d c b b b c b b b c b b b c b
+    b d c c c c c c c c c c c c c b
+    b d c b b b c b b b c b b b c b
+    b d c b b b c b b b c b b b c b
+    b d d d d d d d d d d d d d d b
+    b b b b b b b b b b b b b b b b
+""")
+
+# --- 4. MAPA Y ENTIDADES ---
+
+def generar_mundo():
+    scene.set_background_color(13)
+    # IMPORTANTE: Configurar mapa en el editor a 20x15
+    tiles.set_current_tilemap(tilemap("""level1"""))
+    
+    # N = NPC, S = Start, P = Pocion
+    nivel = [
+        "WWWWWWWWWWWWWWWWWWWW",
+        "W.S..N.............W",
+        "W.WWWWWWW.WW.WWWWW.W",
+        "W.W.....W.WW.W...W.W",
+        "W.W.1...W.WW.W.2.W.W",
+        "W.WWWWWWW.WW.WWWWW.W",
+        "W.........E........W",
+        "WWWWWWWW.....WWWWWWW",
+        "W......E...........W",
+        "W.WWWWWWW...WWWWWW.W",
+        "W.W.....W...W....W.W",
+        "W.W.3.......W..C.W.W",
+        "W.WWWWWWWW.WWWWWWW.W",
+        "W.........P........W",
+        "WWWWWWWWWWWWWWWWWWWW"
+    ]
+    
+    filas = len(nivel)
+    cols = len(nivel[0])
+    
+    for r in range(filas):
+        fila = nivel[r]
+        for c in range(cols):
+            char = fila[c]
+            loc = tiles.get_tile_location(c, r)
+            
+            # 1. Suelo
+            tiles.set_tile_at(loc, img_suelo)
+            
+            # 2. Elementos
+            if char == "W":
+                tiles.set_tile_at(loc, img_pared)
+                tiles.set_wall_at(loc, True)
+            
+            elif char == "C":
+                caldero = sprites.create(img_caldero, KIND_META)
+                tiles.place_on_tile(caldero, loc)
+                caldero.start_effect(effects.fountain, 50000)
+            
+            elif char == "S":
+                tiles.place_on_tile(jugador, loc)
+            
+            elif char == "N":
+                crear_rey(loc)
+
+            elif char == "E":
+                crear_enemigo(loc)
+                
+            elif char == "P":
+                crear_item("Pocion Salud", img_salud, loc, "curacion")
+            
+            elif char == "1":
+                crear_item("Gema Magica", img_gema, loc, "mision")
+            elif char == "2":
+                crear_item("Hierba Santa", img_planta, loc, "mision")
+            elif char == "3":
+                crear_item("Libro Antiguo", img_libro, loc, "mision")
+
+def crear_rey(loc: tiles.Location):
+    global rey_npc
+    rey_npc = sprites.create(img_rey, KIND_NPC)
+    tiles.place_on_tile(rey_npc, loc)
+    rey_npc.start_effect(effects.smiles, 50000)
+
+def crear_enemigo(loc: tiles.Location):
+    ene = sprites.create(img_fantasma, KIND_ENEMIGO)
+    tiles.place_on_tile(ene, loc)
+    ene.follow(jugador, 25)
+
+def crear_item(nombre: str, img_obj: Image, loc: tiles.Location, tipo: str):
+    # Items de curación no se guardan
+    if tipo == "curacion":
+        spr = sprites.create(img_obj, KIND_ITEM)
+        tiles.place_on_tile(spr, loc)
+        nuevo_item = ItemJuego(nombre, img_obj, tipo)
+        nuevo_item.sprite_fisico = spr
+        items.append(nuevo_item)
+        return
+
+    # Items de misión
+    nuevo_item = ItemJuego(nombre, img_obj, tipo)
+    items.append(nuevo_item)
+    
+    if settings.read_number("got_" + nombre) == 1:
+        nuevo_item.recogido = True
+    else:
+        spr = sprites.create(img_obj, KIND_ITEM)
+        tiles.place_on_tile(spr, loc)
+        spr.start_effect(effects.halo, 50000)
+        nuevo_item.sprite_fisico = spr
+
+def setup_hero():
+    global jugador
+    jugador = sprites.create(img_hero, SpriteKind.player)
+    controller.move_sprite(jugador, 80, 80)
+    scene.camera_follow_sprite(jugador)
+    jugador.set_stay_in_screen(True)
+
+# --- 5. LÓGICA DEL JUEGO ---
+
+def bucle_principal():
+    global energia, juego_activo
+    
+    if not juego_activo: return
+
+    velocidad = 80
+    gasto = 0.05
+
+    # SPRINT (Correr)
+    if controller.A.is_pressed():
+        velocidad = 120
+        gasto = 0.2
+        jugador.start_effect(effects.trail, 100)
+    
+    controller.move_sprite(jugador, velocidad, velocidad)
+
+    # Gasto de energía
+    if controller.left.is_pressed() or controller.right.is_pressed() or controller.up.is_pressed() or controller.down.is_pressed():
+        energia -= gasto
+
+    # HUD (Sin cambiar color para evitar errores)
+    info.set_score(int(energia))
+    
+    if energia <= 0:
+        game.over(False, effects.melt)
+
+game.on_update(bucle_principal)
+
+# INTERACCIÓN NPC
+def on_npc_overlap(player, npc):
+    global mision_iniciada
+    if not mision_iniciada:
+        game.show_long_text("REY: ¡Alquimista!\nUna plaga destruye mi reino.\nEncuentra los 3 objetos sagrados y llevalos al caldero.", DialogLayout.BOTTOM)
+        game.show_long_text("REY: Ten cuidado con los fantasmas.\n¡Ve y salvanos!", DialogLayout.BOTTOM)
+        mision_iniciada = True
+        player.y += 16
+
+sprites.on_overlap(SpriteKind.player, KIND_NPC, on_npc_overlap)
+
+# INTERACCIÓN ITEMS
+def on_item_overlap(player, other):
+    global energia
+    for it in items:
+        if it.sprite_fisico == other:
+            
+            if it.tipo == "curacion":
+                energia = min(100, energia + 30)
+                other.destroy(effects.hearts, 500)
+                music.power_up.play()
+                player.say("Recuperado!", 500)
+                # No usamos remove, solo lo marcamos como recogido
+                it.recogido = True
+                break
+
+            if it.tipo == "mision":
+                it.recogido = True
+                other.destroy(effects.confetti, 500)
+                music.magic_wand.play()
+                energia = min(100, energia + 10)
+                game.show_long_text("¡Conseguido!\n" + it.nombre, DialogLayout.BOTTOM)
+                settings.write_number("got_" + it.nombre, 1)
+                break
+
+sprites.on_overlap(SpriteKind.player, KIND_ITEM, on_item_overlap)
+
+# INTERACCIÓN ENEMIGOS
+def on_enemy_overlap(player, enemy):
+    global energia
+    energia -= 5
+    scene.camera_shake(4, 200)
+    music.zapped.play()
+    
+    if player.x < enemy.x:
+        player.x -= 16
+    else:
+        player.x += 16
+    
+    player.say("¡Ay!", 500)
+
+sprites.on_overlap(SpriteKind.player, KIND_ENEMIGO, on_enemy_overlap)
+
+# INTERACCIÓN META
+def on_meta_overlap(player, meta):
+    faltan = False
+    for i in items:
+        if i.tipo == "mision" and not i.recogido:
+            faltan = True
+    
+    if not faltan:
+        music.ba_ding.play()
+        game.over(True, effects.star_field)
+    else:
+        player.say("Necesito los 3 objetos!", 1000)
+        player.y -= 10
+
+sprites.on_overlap(SpriteKind.player, KIND_META, on_meta_overlap)
+
+# --- 6. MENÚS Y ARRANQUE ---
+
+def mostrar_inventari():
+    texto = "MOCHILA:\n"
+    for i in items:
+        if i.tipo == "mision":
+            estado = "[X] " if i.recogido else "[ ] "
+            texto += estado + i.nombre + "\n"
+    game.show_long_text(texto, DialogLayout.FULL)
+
+def inicio():
+    global juego_activo, energia
+    game.splash("THE ALCHEMIST", "Platinum Edition")
+    
+    opcion = game.ask_for_number("1. Jugar\n2. Borrar Progreso", 1)
+    
+    if opcion == 2:
+        settings.write_number("got_Gema Magica", 0)
+        settings.write_number("got_Hierba Santa", 0)
+        settings.write_number("got_Libro Antiguo", 0)
+        game.splash("Memoria borrada")
+
+    setup_hero()
+    # Botón B activa inventario
+    controller.B.on_event(ControllerButtonEvent.PRESSED, mostrar_inventari)
+    
+    generar_mundo()
+    energia = 100.0
+    juego_activo = True
+    
+    game.show_long_text("CONTROLES:\nA = Correr\nB = Inventario\n\nHabla con el REY para empezar.", DialogLayout.FULL)
+
+# ¡ARRANCAR MOTOR!
+inicio()
--- main.ts
+++ main.ts
@@ -1,35 +1,412 @@
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`nena-animation-up`,
-    500,
-    false
-    )
+/** 👑 THE ALCHEMIST👑 */
+//  --- 1. CLASES ---
+class ItemJuego {
+    nombre: string
+    imagen: Image
+    tipo: string
+    recogido: boolean
+    sprite_fisico: Sprite
+    constructor(nombre: string, imagen: Image, tipo: string) {
+        this.nombre = nombre
+        this.imagen = imagen
+        this.tipo = tipo
+        //  "mision" o "curacion"
+        this.recogido = false
+        this.sprite_fisico = null
+    }
+    
+}
+
+//  --- 2. CONFIGURACIÓN ---
+let items : ItemJuego[] = []
+let enemigos : Sprite[] = []
+let jugador : Sprite = null
+let rey_npc : Sprite = null
+let energia = 100.0
+let juego_activo = false
+let mision_iniciada = false
+//  Tipos de Sprite
+let KIND_ITEM = SpriteKind.create()
+let KIND_META = SpriteKind.create()
+let KIND_ENEMIGO = SpriteKind.Enemy
+let KIND_NPC = SpriteKind.create()
+//  --- 3. ARTE PIXEL ---
+let img_hero = img`
+    . . . . 2 2 2 2 . . . .
+    . . . 2 2 2 2 2 2 . . .
+    . . 2 2 2 2 2 2 2 2 . .
+    . . 2 2 8 8 8 8 2 2 . .
+    . . 2 8 8 8 8 8 8 2 . .
+    . . 8 8 f f f f 8 8 . .
+    . . 8 f f 2 2 f f 8 . .
+    . . 8 f f f f f f 8 . .
+    . . 8 8 8 8 8 8 8 8 . .
+    . . f 8 8 8 8 8 8 f . .
+    . . f 8 5 5 5 5 8 f . .
+    . . . 8 5 5 5 5 8 . . .
+    . . . f f . . f f . . .
+    . . . . . . . . . . . .
+`
+let img_rey = img`
+    . . . . 5 5 5 5 . . . .
+    . . . 5 5 4 4 5 5 . . .
+    . . 5 5 4 5 5 4 5 5 . .
+    . . 5 5 4 4 4 4 5 5 . .
+    . . . f f f f f f . . .
+    . . . f f 2 2 f f . . .
+    . . . f f f f f f . . .
+    . . . 5 5 5 5 5 5 . . .
+    . . . 5 4 5 5 4 5 . . .
+    . . . 5 5 5 5 5 5 . . .
+    . . . 5 5 5 5 5 5 . . .
+    . . . 5 5 . . 5 5 . . .
+    . . . . . . . . . . . .
+    . . . . . . . . . . . .
+`
+let img_fantasma = img`
+    . . . . . . . . . . . .
+    . . . . 1 1 1 1 . . . .
+    . . . 1 1 1 1 1 1 . . .
+    . . 1 1 1 1 1 1 1 1 . .
+    . . 1 1 f 1 1 f 1 1 . .
+    . . 1 1 1 1 1 1 1 1 . .
+    . . 1 1 1 1 1 1 1 1 . .
+    . . 1 1 1 1 1 1 1 1 . .
+    . . 1 1 1 1 1 1 1 1 . .
+    . . 1 . 1 . 1 . 1 . . .
+    . . . . . . . . . . . .
+`
+let img_gema = img`
+    . . . . . . . . . . . .
+    . . . . . 2 2 . . . . .
+    . . . . 2 4 4 2 . . . .
+    . . . 2 4 2 2 4 2 . . .
+    . . . 2 4 2 2 4 2 . . .
+    . . . 2 4 4 4 4 2 . . .
+    . . . . 2 4 4 2 . . . .
+    . . . . . 2 2 . . . . .
+    . . . . . . . . . . . .
+`
+let img_planta = img`
+    . . . . . . . . . . . .
+    . . . . . . 7 . . . . .
+    . . . . . 7 7 . . . . .
+    . . . 7 . 7 7 . 7 . . .
+    . . . 7 7 7 7 7 7 . . .
+    . . . . 7 7 7 7 . . . .
+    . . . . 7 7 7 7 . . . .
+    . . . . . 7 7 . . . . .
+    . . . . . . . . . . . .
+`
+let img_libro = img`
+    . . . . . . . . . . . .
+    . . . b b b b . . . . .
+    . . b 1 1 1 1 b . . . .
+    . b 1 1 1 1 1 1 b . . .
+    . b c c c c c c b . . .
+    . b c c c c c c b . . .
+    . b c c c c c c b . . .
+    . b 1 1 1 1 1 1 b . . .
+    . . b b b b b b . . . .
+`
+let img_salud = img`
+    . . . . . . . . . . . .
+    . . . . . 1 1 . . . . .
+    . . . . . 1 1 . . . . .
+    . . . . 1 1 1 1 . . . .
+    . . . 1 2 2 2 2 1 . . .
+    . . . 1 2 2 2 2 1 . . .
+    . . 1 2 2 2 2 2 2 1 . .
+    . . 1 2 f f f 2 2 1 . .
+    . . 1 2 2 2 2 2 2 1 . .
+    . . . 1 2 2 2 2 1 . . .
+    . . . . 1 1 1 1 . . . .
+`
+let img_caldero = img`
+    . . . . . . . . . . . .
+    . . . b b . . b b . . .
+    . . b 1 1 b b 1 1 b . .
+    . b 1 1 1 1 1 1 1 1 b .
+    . b 1 2 2 5 5 2 2 1 b .
+    . b 1 2 5 5 5 5 2 1 b .
+    . b b 2 5 5 5 5 2 b b .
+    . . b 2 2 5 5 2 2 b . .
+    . . . b b b b b b . . .
+    . . . d . . . . d . . .
+    . . d . . . . . . d . .
+`
+let img_suelo = img`
+    c c b c c c b c c c b c c c b c
+    c c b c c c b c c c b c c c b c
+    b b b b b b b b b b b b b b b b
+    c b c c c b c c c b c c c b c c
+    c b c c c b c c c b c c c b c c
+    b b b b b b b b b b b b b b b b
+    c c c b c c c b c c c b c c c b
+    c c c b c c c b c c c b c c c b
+    b b b b b b b b b b b b b b b b
+    c c b c c c b c c c b c c c b c
+    c c b c c c b c c c b c c c b c
+    b b b b b b b b b b b b b b b b
+    c b c c c b c c c b c c c b c c
+    c b c c c b c c c b c c c b c c
+    b b b b b b b b b b b b b b b b
+    c c c b c c c b c c c b c c c b
+`
+let img_pared = img`
+    b b b b b b b b b b b b b b b b
+    b d d d d d d d d d d d d d d b
+    b d c c c c c c c c c c c c d b
+    b d c b b b c b b b c b b b c b
+    b d c b b b c b b b c b b b c b
+    b d c c c c c c c c c c c c c b
+    b d c b b b c b b b c b b b c b
+    b d c c c c c c c c c c c c c b
+    b d c c c c c c c c c c c c c b
+    b d c b b b c b b b c b b b c b
+    b d c b b b c b b b c b b b c b
+    b d c c c c c c c c c c c c c b
+    b d c b b b c b b b c b b b c b
+    b d c b b b c b b b c b b b c b
+    b d d d d d d d d d d d d d d b
+    b b b b b b b b b b b b b b b b
+`
+//  --- 4. MAPA Y ENTIDADES ---
+function generar_mundo() {
+    let fila: string;
+    let char: string;
+    let loc: tiles.Location;
+    let caldero: Sprite;
+    scene.setBackgroundColor(13)
+    //  IMPORTANTE: Configurar mapa en el editor a 20x15
+    tiles.setCurrentTilemap(tilemap`level1`)
+    //  N = NPC, S = Start, P = Pocion
+    let nivel = ["WWWWWWWWWWWWWWWWWWWW", "W.S..N.............W", "W.WWWWWWW.WW.WWWWW.W", "W.W.....W.WW.W...W.W", "W.W.1...W.WW.W.2.W.W", "W.WWWWWWW.WW.WWWWW.W", "W.........E........W", "WWWWWWWW.....WWWWWWW", "W......E...........W", "W.WWWWWWW...WWWWWW.W", "W.W.....W...W....W.W", "W.W.3.......W..C.W.W", "W.WWWWWWWW.WWWWWWW.W", "W.........P........W", "WWWWWWWWWWWWWWWWWWWW"]
+    let filas = nivel.length
+    let cols = nivel[0].length
+    for (let r = 0; r < filas; r++) {
+        fila = nivel[r]
+        for (let c = 0; c < cols; c++) {
+            char = fila[c]
+            loc = tiles.getTileLocation(c, r)
+            //  1. Suelo
+            tiles.setTileAt(loc, img_suelo)
+            //  2. Elementos
+            if (char == "W") {
+                tiles.setTileAt(loc, img_pared)
+                tiles.setWallAt(loc, true)
+            } else if (char == "C") {
+                caldero = sprites.create(img_caldero, KIND_META)
+                tiles.placeOnTile(caldero, loc)
+                caldero.startEffect(effects.fountain, 50000)
+            } else if (char == "S") {
+                tiles.placeOnTile(jugador, loc)
+            } else if (char == "N") {
+                crear_rey(loc)
+            } else if (char == "E") {
+                crear_enemigo(loc)
+            } else if (char == "P") {
+                crear_item("Pocion Salud", img_salud, loc, "curacion")
+            } else if (char == "1") {
+                crear_item("Gema Magica", img_gema, loc, "mision")
+            } else if (char == "2") {
+                crear_item("Hierba Santa", img_planta, loc, "mision")
+            } else if (char == "3") {
+                crear_item("Libro Antiguo", img_libro, loc, "mision")
+            }
+            
+        }
+    }
+}
+
+function crear_rey(loc: tiles.Location) {
+    
+    rey_npc = sprites.create(img_rey, KIND_NPC)
+    tiles.placeOnTile(rey_npc, loc)
+    rey_npc.startEffect(effects.smiles, 50000)
+}
+
+function crear_enemigo(loc: tiles.Location) {
+    let ene = sprites.create(img_fantasma, KIND_ENEMIGO)
+    tiles.placeOnTile(ene, loc)
+    ene.follow(jugador, 25)
+}
+
+function crear_item(nombre: string, img_obj: Image, loc: tiles.Location, tipo: string) {
+    let spr: Sprite;
+    let nuevo_item: ItemJuego;
+    //  Items de curación no se guardan
+    if (tipo == "curacion") {
+        spr = sprites.create(img_obj, KIND_ITEM)
+        tiles.placeOnTile(spr, loc)
+        nuevo_item = new ItemJuego(nombre, img_obj, tipo)
+        nuevo_item.sprite_fisico = spr
+        items.push(nuevo_item)
+        return
+    }
+    
+    //  Items de misión
+    nuevo_item = new ItemJuego(nombre, img_obj, tipo)
+    items.push(nuevo_item)
+    if (settings.readNumber("got_" + nombre) == 1) {
+        nuevo_item.recogido = true
+    } else {
+        spr = sprites.create(img_obj, KIND_ITEM)
+        tiles.placeOnTile(spr, loc)
+        spr.startEffect(effects.halo, 50000)
+        nuevo_item.sprite_fisico = spr
+    }
+    
+}
+
+function setup_hero() {
+    
+    jugador = sprites.create(img_hero, SpriteKind.Player)
+    controller.moveSprite(jugador, 80, 80)
+    scene.cameraFollowSprite(jugador)
+    jugador.setStayInScreen(true)
+}
+
+//  --- 5. LÓGICA DEL JUEGO ---
+game.onUpdate(function bucle_principal() {
+    
+    if (!juego_activo) {
+        return
+    }
+    
+    let velocidad = 80
+    let gasto = 0.05
+    //  SPRINT (Correr)
+    if (controller.A.isPressed()) {
+        velocidad = 120
+        gasto = 0.2
+        jugador.startEffect(effects.trail, 100)
+    }
+    
+    controller.moveSprite(jugador, velocidad, velocidad)
+    //  Gasto de energía
+    if (controller.left.isPressed() || controller.right.isPressed() || controller.up.isPressed() || controller.down.isPressed()) {
+        energia -= gasto
+    }
+    
+    //  HUD (Sin cambiar color para evitar errores)
+    info.setScore(Math.trunc(energia))
+    if (energia <= 0) {
+        game.over(false, effects.melt)
+    }
+    
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`nena-animation-left`,
-    500,
-    false
-    )
+//  INTERACCIÓN NPC
+sprites.onOverlap(SpriteKind.Player, KIND_NPC, function on_npc_overlap(player: Sprite, npc: Sprite) {
+    
+    if (!mision_iniciada) {
+        game.showLongText(`REY: ¡Alquimista!
+Una plaga destruye mi reino.
+Encuentra los 3 objetos sagrados y llevalos al caldero.`, DialogLayout.Bottom)
+        game.showLongText(`REY: Ten cuidado con los fantasmas.
+¡Ve y salvanos!`, DialogLayout.Bottom)
+        mision_iniciada = true
+        player.y += 16
+    }
+    
 })
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`nena-animation-right`,
-    500,
-    false
-    )
+//  INTERACCIÓN ITEMS
+sprites.onOverlap(SpriteKind.Player, KIND_ITEM, function on_item_overlap(player: Sprite, other: Sprite) {
+    
+    for (let it of items) {
+        if (it.sprite_fisico == other) {
+            if (it.tipo == "curacion") {
+                energia = Math.min(100, energia + 30)
+                other.destroy(effects.hearts, 500)
+                music.powerUp.play()
+                player.say("Recuperado!", 500)
+                //  No usamos remove, solo lo marcamos como recogido
+                it.recogido = true
+                break
+            }
+            
+            if (it.tipo == "mision") {
+                it.recogido = true
+                other.destroy(effects.confetti, 500)
+                music.magicWand.play()
+                energia = Math.min(100, energia + 10)
+                game.showLongText("¡Conseguido!\n" + it.nombre, DialogLayout.Bottom)
+                settings.writeNumber("got_" + it.nombre, 1)
+                break
+            }
+            
+        }
+        
+    }
 })
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`nena-animation-down`,
-    500,
-    false
-    )
+//  INTERACCIÓN ENEMIGOS
+sprites.onOverlap(SpriteKind.Player, KIND_ENEMIGO, function on_enemy_overlap(player: Sprite, enemy: Sprite) {
+    
+    energia -= 5
+    scene.cameraShake(4, 200)
+    music.zapped.play()
+    if (player.x < enemy.x) {
+        player.x -= 16
+    } else {
+        player.x += 16
+    }
+    
+    player.say("¡Ay!", 500)
 })
-let nena: Sprite = null
-nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
-controller.moveSprite(nena)
+//  INTERACCIÓN META
+sprites.onOverlap(SpriteKind.Player, KIND_META, function on_meta_overlap(player: Sprite, meta: Sprite) {
+    let faltan = false
+    for (let i of items) {
+        if (i.tipo == "mision" && !i.recogido) {
+            faltan = true
+        }
+        
+    }
+    if (!faltan) {
+        music.baDing.play()
+        game.over(true, effects.starField)
+    } else {
+        player.say("Necesito los 3 objetos!", 1000)
+        player.y -= 10
+    }
+    
+})
+//  --- 6. MENÚS Y ARRANQUE ---
+function inicio() {
+    
+    game.splash("THE ALCHEMIST", "Platinum Edition")
+    let opcion = game.askForNumber(`1. Jugar
+2. Borrar Progreso`, 1)
+    if (opcion == 2) {
+        settings.writeNumber("got_Gema Magica", 0)
+        settings.writeNumber("got_Hierba Santa", 0)
+        settings.writeNumber("got_Libro Antiguo", 0)
+        game.splash("Memoria borrada")
+    }
+    
+    setup_hero()
+    //  Botón B activa inventario
+    controller.B.onEvent(ControllerButtonEvent.Pressed, function mostrar_inventari() {
+        let estado: any;
+        let texto = "MOCHILA:\n"
+        for (let i of items) {
+            if (i.tipo == "mision") {
+                estado = i.recogido ? "[X] " : "[ ] "
+                texto += estado + i.nombre + "\n"
+            }
+            
+        }
+        game.showLongText(texto, DialogLayout.Full)
+    })
+    generar_mundo()
+    energia = 100.0
+    juego_activo = true
+    game.showLongText(`CONTROLES:
+A = Correr
+B = Inventario
+
+Habla con el REY para empezar.`, DialogLayout.Full)
+}
+
+//  ¡ARRANCAR MOTOR!
+inicio()
